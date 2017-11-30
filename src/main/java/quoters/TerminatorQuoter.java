e8a5b84ba7f24b28daa6e0b63ae38fd3988d16e1
Sergei Morozov <sudostan@gmail.com>
2017-11-30 21:10:09 +0300

    TerminatorQuoter bad constructor

--- src/main/java/quoters/TerminatorQuoter.java
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -4,6 +4,10 @@ public class TerminatorQuoter implements Quoter{
     @InjectRandomInt(min = 2, max = 7)
     private int repeat;
 
+    public TerminatorQuoter() {
+        System.out.println(repeat);
+    }
+
     private String message;
 
     public void setMessage(String message) {
